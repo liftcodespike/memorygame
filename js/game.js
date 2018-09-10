0a67fa81c2955a72b37075336cf0e930f1a08409
Ray Bell <raybell@MacBook-Pro-3.local>
2018-09-09 19:45:52 -0700

    Added createAndSetLiItems

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ class Game{
 	constructor(){
 
 	}
+
 	//returns a list of shuffled items.
 	getShuffledItems(){
 		var items = ["automobile","bell","bicycle", "bolt", "bomb","check", "futbol-o","gamepad"];
@@ -14,13 +15,24 @@ class Game{
 		}
 		return items;
 	}
+
+	// fully creates a li  element to add.
+	createAndSetLiItem(itemsArr, index){
+		let li = document.createElement('li');
+		li.setAttribute('card-type', `fa fa-${itemsArr[index]}`);
+		return li;
+	}
+
 	// this function returns a randomized unordered list inside a document frag to add to page.
 	createDocFrag(){
-		let items1 = getShuffledItems()
+		//we create to list Items arr so we can get two items of each in a random order.
+		let items1 = getShuffledItems();
+		let items2 = getShuffledItems();
 		let fragment = document.createDocumentFragment();
 		let ul = document.createElement('ul');
-		for(let index = 0; index< this.items.length; index +=1){
-			ul.appendChild(<)
+		for(let index = 0; index< this.items1.length; index +=1){
+			ul.appendChild(createAndSetLiItem(items1, index));
+			ul.appendChild(createAndSetLiItem(items2, index));
 		}
 	}
 }
