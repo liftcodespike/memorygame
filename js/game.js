4daf8de37c99e5b5b494fc09e0038b7582f62072
Ray Bell <raybell@MacBook-Pro-3.domain>
2018-09-09 21:43:25 -0700

    added css for small screens

--- js/game.js
+++ js/game.js
@@ -19,20 +19,23 @@ class Game{
 	// fully creates a li  element to add.
 	createAndSetLiItem(itemsArr, index){
 		let li = document.createElement('li');
-		li.setAttribute('card-type', `fa fa-${itemsArr[index]}`);
+		li.setAttribute('class', 'hidden');
+		li.setAttribute('class', `fa fa-${itemsArr[index]} fa-2x`);
 		return li;
 	}
 
 	// this function returns a randomized unordered list inside a document frag to add to page.
-	createDocFrag(){
+	createCards(){
 		//we create to list Items arr so we can get two items of each in a random order.
-		let items1 = getShuffledItems();
-		let items2 = getShuffledItems();
+		let items1 = this.getShuffledItems();
+		let items2 = this.getShuffledItems();
 		let fragment = document.createDocumentFragment();
 		let ul = document.createElement('ul');
-		for(let index = 0; index< this.items1.length; index +=1){
-			ul.appendChild(createAndSetLiItem(items1, index));
-			ul.appendChild(createAndSetLiItem(items2, index));
+		for(let index = 0; index< items1.length; index +=1){
+			ul.appendChild(this.createAndSetLiItem(items1, index));
+			ul.appendChild(this.createAndSetLiItem(items2, index));
 		}
+		fragment.appendChild(ul);
+		return fragment;
 	}
 }
